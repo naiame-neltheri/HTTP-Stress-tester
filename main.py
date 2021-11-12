64c3781314e9dd6de6de7d6ba5c9c03b3580833b
naiame-neltheri <naiame.pb@gmail.com>
2021-11-12 19:15:13 +0800

    Removed unnecessary files

--- main.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
-
-
-while True:
-    msg = s.recv(8)
-    print(msg.decode("utf-8"))
--- manage.py
+++
@@ -1,9 +0,0 @@
-{
- 
-  "scripts": {
- 
-    "start": "gunicorn -b unix:/mnt/home/app.sock --worker-class socketio.sgunicorn.GeventSocketIOWorker server:app"
- 
-  }
- 
-}
